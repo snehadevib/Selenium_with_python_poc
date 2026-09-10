9c50e207142969e4c814e3f7d58c22c3bb30188b
snehadevib <snehadevib@gmail.com>
2026-09-10 13:41:59 +0200

    fix: add explicit waits to cart and checkout pages to prevent CI flakiness

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -8,4 +8,20 @@ class CartPage:
         self.driver = driver
 
     def click_checkout(self):
-        self.driver.find_element(*self.CHECKOUT_BUTTON).click()
+        self.driver.find_element(*self.CHECKOUT_BUTTON).click()from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CartPage:
+    CHECKOUT_BUTTON = (By.ID, "checkout")
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def click_checkout(self):
+        button = self.wait.until(
+            EC.element_to_be_clickable(self.CHECKOUT_BUTTON)
+        )
+        button.click()
--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,30 +1,23 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 class CheckoutPage:
     FIRST_NAME_INPUT = (By.ID, "first-name")
     LAST_NAME_INPUT = (By.ID, "last-name")
     ZIP_CODE_INPUT = (By.ID, "postal-code")
     CONTINUE_BUTTON = (By.ID, "continue")
     FINISH_BUTTON = (By.ID, "finish")
-    CONFIRMATION_HEADER = (By.CSS_SELECTOR, ".complete-header")
+    CONFIRMATION_HEADER = (By.CLASS_NAME, "complete-header")
 
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
     def fill_checkout_info(self, first_name, last_name, zip_code):
-        self.driver.find_element(*self.FIRST_NAME_INPUT).send_keys(first_name)
+        self.wait.until(EC.visibility_of_element_located(self.FIRST_NAME_INPUT)).send_keys(first_name)
         self.driver.find_element(*self.LAST_NAME_INPUT).send_keys(last_name)
         self.driver.find_element(*self.ZIP_CODE_INPUT).send_keys(zip_code)
-
-    def click_continue(self):
-        self.driver.find_element(*self.CONTINUE_BUTTON).click()
+        self.wait.until(EC.element_to_be_clickable(self.CONTINUE_BUTTON)).click()
 
     def click_finish(self):
-        self.driver.find_element(*self.FINISH_BUTTON).click()
+        self.wait.until(EC.element_to_be_clickable(self.FINISH_BUTTON)).click()
 
     def get_confirmation_message(self):
         header = self.wait.until(
